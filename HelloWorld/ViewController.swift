6913ebedba1aeeb5bd885134a0326a7cd19f1d6c
Lucas Costi <lucascosti@users.noreply.github.com>
2021-04-21 13:54:03 +1000

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Lucas Costi on 21/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
